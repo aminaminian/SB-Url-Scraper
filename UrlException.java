7ce069b23092c20723ecd2338e85d5e931ea0023
aminaminian <aminian.amin@gmail.com>
2019-09-29 16:15:27 +0800

    first commit

---
+++ UrlException.java
@@ -0,0 +1,12 @@
+package com.freelancer.scraper.exception;
+
+public class UrlException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UrlException(String s) {
+        super(s);
+    }
+}
